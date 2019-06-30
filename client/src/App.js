5c30e676501b3ee7bafee03b9bea3dee47c85b0c
gk <georgepulikkayath@gmail.com>
2019-06-29 21:16:28 -0400

    files changed

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Bookshelf from "./pages/bookshelf";
+import Search from "./pages/search";
+import NavBar from "./components/NavBar/NavBar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <Router>
+    <div>
+      <NavBar />
+      <Route exact path="/" component={Search} />
+      <Route exact path="/bookshelf" component={Bookshelf} />
+      <Route exact path="/search" component={Search} />
+
     </div>
-  );
-}
+  </Router>
+);
 
 export default App;
